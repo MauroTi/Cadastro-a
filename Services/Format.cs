279123738bb5933ffe9b8e206740ee5adc5aa282
MauroTi <mauro4879@gmail.com>
2024-03-18 16:22:23 -0300

    Adicionar arquivos de projeto.

---
+++ Services/Format.cs
@@ -0,0 +1,17 @@
+﻿namespace Cadastro_a.Services
+{
+        public class FormatacaoService
+        {
+            public string FormatarDados(string nome, string endereco, string telefone)
+            {
+                // Formatar os dados para caixa alta e itálico
+                var nomeFormatado = $"<b><i>{nome.ToUpper()}</i></b>";
+                var enderecoFormatado = $"<b><i>{endereco.ToUpper()}</i></b>";
+                var telefoneFormatado = $"<b><i>{telefone.ToUpper()}</i></b>";
+
+                // Retornar os dados formatados
+                return $"Nome: {nomeFormatado}<br/>Endereço: {enderecoFormatado}<br/>Telefone: {telefoneFormatado}";
+            }
+
+        }
+}
---
+++ Views/Home/Resultados.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Cadastro_a.Views.Home
+{
+    public class Resultados : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
